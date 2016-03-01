f1a7f3b906ac469eed81f277ad522124a666411f
Marvin Frederickson <marvin.frederickson@gmail.com>
2016-03-01 10:57:21 -0900

    make rubocop happy

--- lib/swhd_api.rb
+++ lib/swhd_api.rb
@@ -4,6 +4,7 @@ require 'typhoeus'
 require 'json'
 
 module SwhdApi
+  # main api helper class
   class Manager
     attr_reader :session_id
     attr_reader :url
@@ -11,86 +12,82 @@ module SwhdApi
 
     def initialize(url = nil, options = {})
       raise SwhdApi::MissingUrl if url.nil? || url.empty?
-      # TODO strip trailing slash if any
+      # TODO: strip trailing slash if any
       @url = url
       @options = options
     end
 
     def connect(credentials = {})
-      if credentials.has_key?(:apikey)
-        # HACK for now, remove once they get the sessionKey working as documented
+      if credentials.key?(:apikey)
+        # HACK: remove once SolarWinds get the sessionKey working as documented
         @session_id = credentials[:apikey]
         return
 
-      elsif credentials.has_key?(:username) && credentials.has_key?(:password)
+      elsif credentials.key?(:username) && credentials.key?(:password)
         # use username and password
-        #@session_id = "2"
-      elsif credentials.has_key?(:techkey)
+        # @session_id = "2"
+      elsif credentials.key?(:techkey)
         # use tech's key
-        #@session_id = "3"
+        # @session_id = "3"
       else
-        raise MissingCredentials.new(":apikey or :techkey or :username and :password")
+        raise MissingCredentials, ':apikey or :techkey or :username and :password'
       end
 
-      results = self.request("Session", :get, credentials) #{ "apiKey" => credentials[:apikey] })
-      @session_id = results["sessionKey"]
-     
+      results = request('Session', :get, credentials) # { "apiKey" => credentials[:apikey] })
+      @session_id = results['sessionKey']
     end
 
     def request(resource, method, params = {}, body = {})
-      raise NoSession if @session_id.nil? && resource != "Session"
+      raise NoSession if @session_id.nil? && resource != 'Session'
       raise MissingResource if resource.nil? || resource.empty?
-      
+
       endpoint = "#{@url}/#{resource}"
-      # TODO log a call to solarwinds and find out why no one can do this (other users on their forums)
-      # HACK for now, use apikey instead of session key
-      params["apiKey"] = @session_id
+      # TODO: log a call to solarwinds and find out why no one can do this (other users on their forums)
+      # HACK: for now, use apikey instead of session key
+      params['apiKey'] = @session_id
       payload = @options.dup
-      payload[:params] = params.reject {|k,v| v.nil?}
+      payload[:params] = params.reject { |_k, v| v.nil? }
 
       response =
-      case method
-      when :post
-        payload[:body] = body.to_json
-        Typhoeus::Request.post(endpoint, payload)
-      when :get
-        Typhoeus::Request.get(endpoint, payload)
-      when :put
-        payload[:body] = body.to_json
-        Typhoeus::Request.put(endpoint, payload)
-      when :delete
-        Typhoeus::Request.delete(endpoint, payload)
-      end
+        case method
+        when :post
+          payload[:body] = body.to_json
+          Typhoeus::Request.post(endpoint, payload)
+        when :get
+          Typhoeus::Request.get(endpoint, payload)
+        when :put
+          payload[:body] = body.to_json
+          Typhoeus::Request.put(endpoint, payload)
+        when :delete
+          Typhoeus::Request.delete(endpoint, payload)
+        end
 
       if response.timed_out?
-        raise TimedOut.new(response.body)
-      elsif response.code == 0 
-        raise NoResponse.new(response.body)
+        raise TimedOut, response.body
+      elsif response.code == 0
+        raise NoResponse, response.body
       elsif response.return_code == :partial_file
-        raise PartialFile.new(response.body)
+        raise PartialFile, response.body
       elsif !response.success?
-        if response.code == 401
-          raise AuthenticationFailure.new(response.return_message)
-        else
-          raise RequestFailed.new("Response Code: #{response.code}\nReturn Code: #{response.return_code} - #{response.return_message}\n#{response.body}")
-        end
+        raise AuthenticationFailure, response.return_message if response.code == 401
+        raise RequestFailed, "Response Code: #{response.code}\n
+          Return Code: #{response.return_code} - #{response.return_message}\n
+          #{response.body}"
       end
 
-      unless @logger.nil?
-        @logger.debug response.body
-      end
+      @logger.debug response.body unless @logger.nil?
 
-      results = JSON.parse(response.body)
+      JSON.parse(response.body)
     end
 
-    # if we are fetching (get) and the results are an array, then fetch additional pages until exhausted
+    # fetch all pages of results
     def fetch(resource, params = {})
       method = :get
       page = 1
 
       params[:page] = page
       partial = request(resource, method, params)
-      while (partial.is_a?(Array) && !partial.empty?) do
+      while partial.is_a?(Array) && !partial.empty?
         results ||= []
         results += partial
         page += 1
@@ -99,9 +96,7 @@ module SwhdApi
       end
 
       results = partial unless partial.nil? || partial.empty?
-
       results
     end
-
   end
 end
